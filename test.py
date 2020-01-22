1195a3a5fc3f771798d8781891febc9e51be2c19
Alexandre Lafleur <alexandre.lafleur@usherbrooke.ca>
2020-01-22 14:15:11 -0500

    [TetraBot]: Moving servomotors

--- test.py
+++ test.py
@@ -2,23 +2,37 @@ import pyfirmata
 import time
 
 def main():
-    board = pyfirmata.Arduino('/dev/ttyACM0')
+    board = pyfirmata.Arduino('/dev/cu.usbmodem14401')
 
     it = pyfirmata.util.Iterator(board)
     it.start()
 
     digital_input = board.get_pin('d:10:i')
     led = board.get_pin('d:13:o')
+    
+    servos = []
+    for i in range(4):
+        pin = board.get_pin('d:' + str(i + 2) + ':s')
+        servos.append(pin) 
+        
+
+    i = 1
+    print("Starting ...")
 
     while True:
-        sw = digital_input.read()
-        if sw is True:
-            led.write(1)
-        else:
-            led.write(0)
-        time.sleep(0.1)
+        led.write(1)
+        servos[2].write(30)
+        servos[3].write(30)
+        time.sleep(2)
+        led.write(0)
+        servos[2].write(90)
+        servos[3].write(90)
+        time.sleep(2)
+
+def walk():
+    pass
 
 
 if __name__ == "__main__":
-    # main()
-    print("Testing ...")
+    main()
+    

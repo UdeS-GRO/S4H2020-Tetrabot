17eacfdb15b25f50bde56d061315e7469a95f851
Alexandre Lafleur <alexandre.lafleur@usherbrooke.ca>
2020-01-22 17:24:14 -0500

    [TetraBot]: Update to servo control

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pyfirmata
 import time
+import math
 
 def main():
     board = pyfirmata.Arduino('/dev/cu.usbmodem14401')
@@ -16,23 +17,41 @@ def main():
         servos.append(pin) 
         
 
-    i = 1
     print("Starting ...")
 
     while True:
-        led.write(1)
-        servos[2].write(30)
-        servos[3].write(30)
+        servos[2].write(angle(110))
+        servos[3].write(angle(160))
         time.sleep(2)
-        led.write(0)
-        servos[2].write(90)
-        servos[3].write(90)
+        servos[2].write(angle(160))
+        servos[3].write(angle(220))
         time.sleep(2)
 
-def walk():
-    pass
+
+def angle(angle):
+    return angle * 255 / 360
+
+
+def get_position_from_angles(a1, a2):
+    l1 = 10
+    l2 = 10
+
+    angle_1 = a1
+    x_cuisse = l1 * math.cos(angle_1)
+    y_cuisse = l1 * math.sin(angle_1)
+
+    angle_2 = math.pi - a1 - a2
+    x_mollet = l2 * math.cos(angle_2)
+    y_mollet = l2 * math.sin(angle_2)
+
+    x_tot = x_cuisse + x_mollet
+    y_tot = y_cuisse + y_mollet
+    print(x_tot)
+    print(y_tot)
 
 
 if __name__ == "__main__":
     main()
+    # get_position_from_angles(math.pi / 2, 0)
+    # get_angles_from_position(0, 20)
     

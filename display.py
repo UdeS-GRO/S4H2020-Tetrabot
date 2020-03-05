b0f402498b285b445640e734a7664a33ff68005e
Felix <tousfelix@gmail.com>
2020-03-05 18:37:48 -0500

    Update gui and debugging display
    Modification FAT 05/03/2020
    Tout est fonctionnel!

--- display.py
+++ display.py
@@ -1,23 +1,22 @@
 import math
-from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import *
 from inverse_kinematics import inverse_kinematic
 
 # Sets the robot's physical parameters (this can change with your CAD)
 legs = []
-leg_length = 100
+thight_length = 100
 tibia_length = 100
-offset_x = 200
-offset_y = 50
-
+offset_x = 225
+offset_y = 100
 
 # The window where the animation is displayed
 class AnimationCanvas:
 
     def __init__(self, root, canvas):
         """
-         @param canvas: window for animation visualization
-         @param offset_3d_x and offset_3d_y : offset for 3D visualization
-         """
+        @param canvas: window for animation visualization
+        @param offset_3d_x and offset_3d_y : offset for 3D visualization
+        """
         self.root = root
         self.canvas = canvas
         self.offset_3d_x = 0
@@ -30,22 +29,33 @@ class AnimationCanvas:
 
         for i in range(2):
             for j in range(2):
-                leg = Leg(offset_x + 200 * i - self.offset_3d_x * j, offset_y + self.offset_3d_y * j, leg_length, 0, tibia_length, 0,
-                          j)
+                leg = Leg(offset_x + 200 * i - self.offset_3d_x * j, offset_y + self.offset_3d_y * j, thight_length, 0,
+                          tibia_length, 0,j)
                 legs.append(leg)
                 index += 1
 
         self.canvas.pack(fill=BOTH, expand=1)
 
-    def redraw(self, delete_all=False, draw_trajectory=True):
-        self.canvas.delete("delete")
-        if delete_all:
-            self.canvas.delete("all")
+    # Displays the steps with the trajectory and the step of each joints (with the option of drawing in 3d)
+    def display_step(self, step, draw_trajectory=True, draw_3d=True):
+        """
+        @param step: steps array
+        @param draw_trajectory: boolean option for trajectory visualization
+        @param draw_3d: boolean option for 3D visualization
+        """
+
+        for i, leg_angles in enumerate(step):
+            legs[i].set_angles(leg_angles[0], leg_angles[1])
+
+        # Delete tags before the redraw (display trajectory or no)
+        if draw_trajectory:
+            self.canvas.delete('body', 'shoulder', 'thight', 'tibia', 'knee', 'ground')
+        else:
+            self.canvas.delete('body', 'shoulder', 'thight', 'tibia', 'knee', 'foot', 'ground')
 
         # Shows the legs on the FrameCanvas
         for leg in legs:
             leg.show(self.canvas, draw_trajectory)
-        off_y = 5
         index = 0
         for i in range(2):
             for j in range(2):
@@ -54,42 +64,34 @@ class AnimationCanvas:
                 leg.posy0 = offset_y + self.offset_3d_y * j
                 index += 1
 
-        self.canvas.create_line(0, offset_y + leg_length + tibia_length / 2 + off_y, 1000,
-                                offset_y + leg_length + tibia_length / 2 + off_y)
-        self.canvas.create_line(legs[0].posx0, legs[0].posy0, legs[1].posx0, legs[1].posy0, legs[3].posx0,
-                                legs[3].posy0, legs[2].posx0, legs[2].posy0, legs[2].posx0, legs[2].posy0,
-                                legs[0].posx0, legs[0].posy0)
-        self.canvas.pack(fill=BOTH, expand=1, padx=15)
+        # Ground
+        off_y = 0
+        self.canvas.create_line(0, offset_y + thight_length + tibia_length / 2 + off_y, 10000,
+                                offset_y + thight_length + tibia_length / 2 + off_y, tags='ground')
 
-    # Displays the steps with the trajectory and the step of each joints (with the option of drawing in 3d)
-    def display_step(self, step, draw_trajectory, draw_3d):
-        """
-         @param step: steps array
-         @param draw_trajectory: the mouvement to display on the canvas
-         @param draw_3d: boolean option for 3D visualization
-         """
+        # 2D <--> 3D
         if draw_3d:
+            self.canvas.create_line(legs[0].posx0, legs[0].posy0, legs[1].posx0, legs[1].posy0, legs[3].posx0,
+                                    legs[3].posy0, legs[2].posx0, legs[2].posy0, legs[2].posx0, legs[2].posy0,
+                                    legs[0].posx0, legs[0].posy0, tags='body')
             self.offset_3d_x = 40
             self.offset_3d_y = 40
         else:
+            self.canvas.create_line(legs[2].posx0, legs[2].posy0, legs[0].posx0, legs[0].posy0, tags='body')
             self.offset_3d_x = 0
             self.offset_3d_y = 0
 
-        for i, leg_angles in enumerate(step):
-            legs[i].set_angles(leg_angles[0], leg_angles[1])
-
-        self.redraw(False, draw_trajectory)
-
+        self.canvas.pack(fill=BOTH, expand=1, padx=10)
 
 # Leg class for each joint animation of the robot
 class Leg:
 
     def __init__(self, x, y, length1, angle1, length2, angle2, j):
         """
-         @param posx0 and posy0 : initial positions of X and Y
-         @param length1 and length2 : Physical lengths of the joints (J1,J2) of the robot
-         @param angle1 and angle2 : Angles of each joints (J1,J2) of the robot
-         """
+        @param posx0 and posy0 : initial positions of X and Y
+        @param length1 and length2 : Physical lengths of the joints (J1,J2) of the robot
+        @param angle1 and angle2 : Angles of each joints (J1,J2) of the robot
+        """
         self.posx0 = x
         self.posy0 = y
         self.length1 = length1
@@ -97,9 +99,9 @@ class Leg:
         self.length2 = length2
         self.angle2 = angle2
         if j == 1:
-            self.color = "#fb0"
+            self.color = '#fb0'
         else:
-            self.color = "green"
+            self.color = 'green'
 
         self.width = 5
 
@@ -111,28 +113,26 @@ class Leg:
     # displays the legs on the canvas with the draw_trajectory
     def show(self, canvas, draw_trajectory):
         """
-         @param canvas: window for animation visualization
-         @param draw_trajectory: the mouvement to display on the canvas
-         """
+        @param canvas: window for animation visualization
+        @param draw_trajectory: the mouvement to display on the canvas
+        """
         posx1 = self.posx0 + self.length1 * math.cos(self.angle1)
         posy1 = self.posy0 - self.length1 * math.sin(self.angle1)
 
         posx2 = posx1 + self.length2 * math.cos(self.angle1 + self.angle2)
         posy2 = posy1 - self.length2 * math.sin(self.angle1 + self.angle2)
+
         # Line of the thigh
-        canvas.create_line(self.posx0, self.posy0, posx1, posy1, width=self.width, fill=self.color,
-                           tags="delete")
+        canvas.create_line(self.posx0, self.posy0, posx1, posy1, width=self.width, fill=self.color, tags='thight')
+
         # Line of the tibia
-        canvas.create_line(posx1, posy1, posx2, posy2, width=self.width,
-                           fill=self.color, tags="delete")
-        # Point of the thigh for the trajectory display
-        canvas.create_line(posx1 - 1, posy1 - 1, posx1 + 2, posy1 + 2, width=2, fill='black',
-                           tags="delete")
-        if draw_trajectory:
-            # first Point of the tibia for the trajectory display
-            canvas.create_line(posx2 - 1, posy2 - 1, posx2 + 2,
-                               posy2 + 2, width=2, fill='black')
-        else:
-            # first Point of the tibia if there is no trajectory display
-            canvas.create_line(posx2 - 1, posy2 - 1, posx2 + 2, posy2 + 2, width=2, fill='black',
-                               tags="delete")
+        canvas.create_line(posx1, posy1, posx2, posy2, width=self.width, fill=self.color, tags='tibia')
+
+        # Shoulder points
+        canvas.create_oval(self.posx0 - 2, self.posy0 - 2, self.posx0 + 2, self.posy0 + 2, fill='black', tags='shoulder')
+
+        # Knee points
+        canvas.create_oval(posx1 - 2, posy1 - 2, posx1 + 2, posy1 + 2, fill='black', tags='knee')
+
+        # Foot points
+        canvas.create_oval(posx2 - 2, posy2 - 2, posx2 + 2, posy2 + 2, fill='black', tags='foot')

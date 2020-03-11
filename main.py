9390c1ffb6cad08c0f984c5be0ba824a21c2b391
Alexis TL <trea2513@usherbrooke.ca>
2020-03-11 16:23:17 -0400

    removed unnecessary changes

--- main.py
+++ main.py
@@ -45,12 +45,6 @@ class Controler:
 
             if self.step_index >= len(self.steps):
                 self.step_index = 0
-                # TODO self.redraw(True, draw_trajectory)
-            else:
-                pass
-                # TODO self.redraw(False, draw_trajectory)
-        else:
-            self.step_index = 0
 
         self.gui.root.after(self.delay, self.main_loop)
 

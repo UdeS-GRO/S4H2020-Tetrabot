31929eed3c2c8469c6f9d723383e190977cdebd9
Laflow3r <32850330+Laflow3r@users.noreply.github.com>
2020-04-16 16:17:00 -0400

    [Tetrabot]: Adding Alexis s setup py

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,31 @@
+from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name='Tetrabot',
     version='1.0',
-    packages=[''],
+    packages=[],
+    install_requires=[
+        'numpy>=1.14.5',
+        'tk',
+        'Adafruit-Blinka',
+        'Adafruit-PlatformDetect',
+        'Adafruit-PureIO',
+        'Pillow',
+        'adafruit-circuitpython-busdevice',
+        'adafruit-circuitpython-motor',
+        'adafruit-circuitpython-pca9685',
+        'adafruit-circuitpython-register',
+        'adafruit-circuitpython-servokit',
+        'cmake',
+        'pyusb',
+        'pyftdi',
+        'pyserial',
+        'setuptools'
+    ],
     url='https://github.com/alexandrelafleur/Tetrabot',
-    license='',
-    author='Tetrabot ',
+    license='MIT',
+    author='Tetrabot',
     author_email='',
     description='Quadriped Bot'
 )
